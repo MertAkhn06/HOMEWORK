94fb0af6e0b8b30acc46a4996a1bdf89005d4e72
mertakhnbrn <mboranboran@gmail.com>
2025-05-29 10:33:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include "fileprocceser.h"
 
 
-int updater( myStruct **arr, int first, int second, const char *add);
-int search( myStruct *arr,int size,char *look);
-void printer( myStruct *arr,int size);
-void printerreverse( myStruct *arr,int size);
+int updater( WordFreq **arr, int first, int second, const char *add);
+int search( const WordFreq *arr,int size,const char *look);
+void printer( const WordFreq *arr, int size);
+void printerreverse( const WordFreq *arr,int size);
 
 int main() {
     int n = 0;
@@ -14,21 +14,24 @@ int main() {
     char incrementtype;
     char filename[30];
     char *returninput = NULL;
-    myStruct *arr = (struct myStruct *)malloc(sizeof( myStruct) * (n));
+    WordFreq *arr = ( WordFreq *)malloc(sizeof( WordFreq) * (n));
     if (arr == NULL) {
         printf("Memory allocation error");
         return 0;
     }
 
-    printf("Enter the file name: ");
+    printf("Enter the file name: "); // getting the file name
     scanf("%s",filename);
-    int x = fileread(filename,&returninput);
+    int x = fileread(filename,&returninput); // getting the input string from the function
     if (x == 0) {
         printf("File read error");
         return 0;
     }
     char *token = strtok(returninput," ");
-    while (token != NULL) {
+    while (token != NULL) { //tokenizing and appending to array
+        if (strlen(token)>=25) { // skips very long strings.
+            token = strtok(NULL," ");
+        }
         if (search(arr,n,token)==n+1) {
             updater(&arr,n,n+1,token);
             n++;
@@ -38,8 +41,12 @@ int main() {
         }
         token = strtok(NULL," ");
     }
-    myStruct *alphaarr = (struct myStruct *)malloc(sizeof(myStruct) * (n));
-    myStruct *freqarr = (struct myStruct *)malloc(sizeof(myStruct) * (n));
+    if (n == 0) {
+        printf("Empty File. ");
+        return 0;
+    }
+    WordFreq *alphaarr = (WordFreq  *)malloc(sizeof(WordFreq) * (n));
+    WordFreq *freqarr = ( WordFreq *)malloc(sizeof(WordFreq) * (n));
     for (int i = 0;i<n;i++) {
         alphaarr[i] = arr[i];
     }
@@ -53,7 +60,7 @@ int main() {
     printf("\n");
     printf("Alphabeticly or Frequency Order? (a/f) ");
     scanf(" %c",&choice);
-    if (tolower(choice) == 'a') {
+    if (tolower(choice) == 'a') { // taking the frequency or alphabetically ordering input.
         printf("Enter the order increasing or decreasing: (i/d): ");
         scanf(" %c",&incrementtype);
         if (tolower(incrementtype) == 'i') {
@@ -61,6 +68,10 @@ int main() {
         }
         else if (tolower(incrementtype )== 'd') {
             printerreverse(alphaarr,n);
+        }
+        else {
+            printf("Undefined Command. ");
+            return 0;
         }
     }
     else if (tolower(choice) == 'f') {
@@ -72,24 +83,27 @@ int main() {
         else if (tolower(incrementtype) == 'd') {
             printer(freqarr,n);
         }
+        else {
+            printf("Undefined Command. ");
+            return 0;
+        }
     }
-
-
-
-
-    free(returninput);
-    for (int i = 0; i < n; i++) {
+    else {
+        printf("Undefined Command. ");
+        return 0;
+    }
+    free(returninput); //freeing memory of the file string.
+    for (int i = 0; i < n; i++) { // and the rest of the arrays.
         free(arr[i].c);
     }
     free(arr);
+    free(alphaarr);
+    free(freqarr);
 
-
-
-    return 0;
 }
 
-int updater( myStruct **arr, int first, int second, const char *add) {
-    struct myStruct *temp = (struct myStruct *)realloc(*arr, sizeof( myStruct) * second);
+int updater( WordFreq **arr, int first, int second, const char *add) { // allocating memory for new cells
+    WordFreq *temp =realloc(*arr, sizeof( WordFreq) * second);
     if (temp == NULL) {
         printf("Memory allocation error\n");
         return 0;
@@ -97,8 +111,8 @@ int updater( myStruct **arr, int first, int second, const char *add) {
     *arr = temp;
     for (int i = first; i < second; i++) {
         (*arr)[i].c = strdup(add);
-        if (!(*arr)[i].c) {
-            printf("String allocation error\n");
+        if ((*arr)[i].c==NULL) {
+            printf("Memoru allocation error\n");
             return 0;
         }
         (*arr)[i].x = 1;
@@ -106,24 +120,21 @@ int updater( myStruct **arr, int first, int second, const char *add) {
     return 1;
 }
 
-int search( myStruct *arr,int size,char *look) {
+int search(const WordFreq *arr,const int size,const char *look) {//search function
     for (int i = 0;i<size;i++) {
         if (strcmp((arr[i]).c,look)==0) {
             return i;
         }
     }
-    return size+1;
+    return size+1; // returning size+1 to prevent limit exceed.
 }
-void printer( myStruct *arr,int size) {
+void printer( const WordFreq *arr,const int size) { //printing the input
     for (int i = 0;i<size;i++) {
         printf("%s : %d \n",(arr[i]).c,arr[i].x);
     }
 }
-void printerreverse( myStruct *arr,int size) {
+void printerreverse( const WordFreq *arr,const int size) { // reverse order
     for (int i = size-1;i>=0;i--) {
         printf("%s : %d \n",(arr[i]).c,arr[i].x);
     }
 }
-
-
-
